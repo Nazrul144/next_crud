cf96b33c4817442341f5d48d5f7908be91b720e4
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-06 14:59:24 +0600

    Navbar added

--- src/components/shared/Navbar.jsx
+++ src/components/shared/Navbar.jsx
@@ -1,10 +1,28 @@
+import Link from 'next/link';
 import React from 'react';
 
 const Navbar = () => {
-    const 
+    const navLinks = [
+        {
+            title: 'Home',
+            path: '/'
+        },
+        {
+            title: 'AddProduct',
+            path: '/addProduct'
+        },
+        {
+            title: 'MyProduct',
+            path: '/myProduct'
+        },
+    ]
     return (
         <div>
-            
+            <nav className='flex gap-2 text-xl font-bold justify-center bg-cyan-500 p-4'>
+            {
+                navLinks?.map((navLink)=> <Link key={navLink.path} href={navLink.path}>{navLink.title}</Link>)
+            }
+            </nav>
         </div>
     );
 }

24c5477b876315cc37accbf5385e0290cc3ce829
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-06 15:09:38 +0600

    Footer button hovering added

--- src/components/shared/Footer.jsx
+++ src/components/shared/Footer.jsx
@@ -5,8 +5,8 @@ const Footer = () => {
     return (
         <div className='h-44 bg-orange-600'>
                 <div className='flex gap-3 text-white justify-center pt-20'>
-                    <Link href='/'>Home</Link>
-                    <Link href='/myProduct'>MyProduct</Link>
+                    <Link className='px-4 py-2 rounded-lg bg-red-900 hover:rounded-full' href='/'>Home</Link>
+                    <Link className='px-4 py-2 rounded-lg bg-red-900 hover:rounded-full' href='/myProduct'>MyProduct</Link>
                 </div>
         </div>
     );

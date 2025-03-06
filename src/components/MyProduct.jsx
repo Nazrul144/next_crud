6aba267c9b1ee1a9b1349d0e745c4fef5af7e9e4
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-06 14:13:11 +0600

    Components section added

---
+++ src/components/MyProduct.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const MyProduct = () => {
+    return (
+        <div>
+            <h1>This is my product section</h1>
+        </div>
+    );
+}
+
+export default MyProduct;

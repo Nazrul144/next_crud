d27a4cafb2b1d79254ec23a78a971b027fbb20c4
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-06 23:22:23 +0600

    View Details page created

--- src/components/MyProducts.jsx
+++ src/components/MyProducts.jsx
@@ -8,6 +8,7 @@ import Button from "@mui/material/Button";
 import Typography from "@mui/material/Typography";
 import Image from "next/image";
 import { Rate } from "antd";
+import Link from "next/link";
 
 const MyProducts = () => {
   const [products, setProducts] = useState([]);
@@ -25,30 +26,45 @@ const MyProducts = () => {
 
   return (
     <div>
-      <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 w-full lg:px-16 mt-12">
-        {products?.map((product) => (
-          <Card key={product._id} >
-            <CardMedia component='div' className="flex justify-center items-center ml-24">
-                <Image src={product.image} alt={product.name} width={300} height={300}/>
-            </CardMedia>
-            <CardContent>
-              <Typography gutterBottom component="div">
-                <h3 className=" "><span className="font-extrabold">Brand: </span>{product.name}</h3>
-                <h3 className=" "><span className="font-extrabold">Price:</span>{product.price}</h3>
-                <h3 className=" "> <span className="font-extrabold">Features: </span>{product.features?.map((feature, index)=>(
-                    <li key={index}>{feature}</li>
-                ))}</h3>
-                <h3 className=" "><span className="font-extrabold">Guarantee: </span>{product.guarantee}</h3>
-                <Rate allowHalf value={product.rating}/>
-              </Typography>
-            </CardContent>
-            <CardActions className="flex justify-between">
-              <Button size="small">Update</Button>
-              <Button size="small">Delete</Button>
-            </CardActions>
-          </Card>
-        ))}
-      </div>
+      <Link href={""}>
+        <div className="grid grid-cols-1 lg:grid-cols-3 gap-6 w-full lg:px-16 mt-12">
+          {products?.map((product) => (
+            <Card key={product._id}>
+              <CardMedia component="div" className="flex justify-center items-center ml-24">
+                <Image src={product.image} alt={product.name} width={300} height={300} />
+              </CardMedia>
+              <CardContent>
+                <Typography gutterBottom component="div">
+                  <h3 className=" ">
+                    <span className="font-extrabold">Brand: </span>
+                    {product.name}
+                  </h3>
+                  <h3 className=" ">
+                    <span className="font-extrabold">Price:</span>
+                    {product.price}
+                  </h3>
+                  <h3 className=" ">
+                    {" "}
+                    <span className="font-extrabold">Features: </span>
+                    {product.features?.map((feature, index) => (
+                      <li key={index}>{feature}</li>
+                    ))}
+                  </h3>
+                  <h3 className=" ">
+                    <span className="font-extrabold">Guarantee: </span>
+                    {product.guarantee}
+                  </h3>
+                  <Rate allowHalf value={product.rating} />
+                </Typography>
+              </CardContent>
+              <CardActions className="flex justify-between">
+                <Button size="small">Update</Button>
+                <Button size="small">Delete</Button>
+              </CardActions>
+            </Card>
+          ))}
+        </div>
+      </Link>
     </div>
   );
 };

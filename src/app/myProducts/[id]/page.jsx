e8953efb103175a9f88ca6b55c429c382ac92fed
Nazrul144 <nazrulislam.cse28@gmail.com>
2025-03-07 01:50:50 +0600

    View Details page added

--- src/app/myProducts/[id]/page.jsx
+++ src/app/myProducts/[id]/page.jsx
@@ -1,22 +1,58 @@
-import React from "react";
+'use client'
+import { Rate } from "antd";
+import Image from "next/image";
+import { useParams } from "next/navigation";
+import React, { useEffect, useState } from "react";
+const DetailsPage = () => {
 
-const Page = () => {
+  const params = useParams();
+  const id = params.id;
+  
+  const [products, setProducts] = useState([])
+  const [loading, setLoading] = useState(true)
+
+  useEffect(()=>{
+    const getSingleData = async()=>{
+      const res = await fetch(`http://localhost:3000/myProducts/api/${id}`)
+      const data = await res.json();
+      setProducts(data.res);
+      setLoading(false);
+    }
+    getSingleData();
+  },[])
+
+  console.log("Full Product Data:", products);
+console.log("Product Image:", products?.image);
+
+
+
+
+
+ 
   return (
     <div>
+      <div>
+        {loading && <span>Loading...</span>}
+      </div>
       <section className="dark:bg-gray-100 dark:text-gray-800">
         <div className="container flex flex-col justify-center p-6 mx-auto sm:py-12 lg:py-24 lg:flex-row lg:justify-between">
           <div className="flex items-center justify-center p-6 mt-8 lg:mt-0 h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128">
-            <img src="assets/svg/Business_SVG.svg" alt="" className="object-contain h-72 sm:h-80 lg:h-96 xl:h-112 2xl:h-128" />
+           <Image src={products?.image} alt={products?.name} width={ 500} height={500}/>
           </div>
           <div className="flex flex-col justify-center p-6 text-center rounded-sm lg:max-w-md xl:max-w-lg lg:text-left">
-            <h1 className="text-5xl font-bold leading-none sm:text-6xl">
-              Ac mattis
-              <span className="dark:text-violet-600">senectus</span>erat pharetra
+            <h1 className="text-2xl font-bold leading-none sm:text-3xl">
+             {name}
             </h1>
             <p className="mt-6 mb-8 text-lg sm:mb-12">
-              Dictum aliquam porta in condimentum ac integer
-              <br className="hidden md:inline lg:hidden" />
-              turpis pulvinar, est scelerisque ligula sem
+              <li>Category:{products?.category}</li>
+              <li>Price:{products?.price}</li>
+              <li>Guarantee:{products?.guarantee}</li>
+              <li className="text-green-500">Features:
+                {products?.features?.map((feature, index)=>(
+                  <li key={index}>{feature}</li>
+                ))}
+              </li>
+              <li>Rating: <Rate allowHalf value={products.rating} /></li>
             </p>
             <div className="flex flex-col space-y-4 sm:items-center sm:justify-center sm:flex-row sm:space-y-0 sm:space-x-4 lg:justify-start">
               <a
@@ -41,4 +77,4 @@ const Page = () => {
   );
 };
 
-export default Page;
+export default DetailsPage;
